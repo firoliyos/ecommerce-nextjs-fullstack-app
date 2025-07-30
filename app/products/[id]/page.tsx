0dd35228ddefbad3737a076ea61d14286d3e8bee
firoliyos <firatesfaye44@gmail.com>
2025-07-30 18:00:28 +0300

    refactor: simplify props destructuring in ProductPage component

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -2,8 +2,7 @@ import { ProductDetail } from "@/components/product-detail";
 import { stripe } from "@/lib/stripe";
 
 
-export default async function ProductPage(props: { params: { id: string } }) {
-  const { params } = await props;
+export default async function ProductPage({ params }: { params: { id: string } }) {
   const product = await stripe.products.retrieve(params.id, {
     expand: ["default_price"],
   });
